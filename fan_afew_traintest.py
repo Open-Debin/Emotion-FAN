0ec7b6376ef015d400d1ccfd873a931346191088
Debin Meng <52323137+Open-Debin@users.noreply.github.com>
2021-05-10 09:13:34 +0800

    Update fan_afew_traintest.py

--- fan_afew_traintest.py
+++ fan_afew_traintest.py
@@ -41,7 +41,7 @@ def main():
     ''' Train & Eval '''
     if args.evaluate == True:
         logger.print('args.evaluate: {:}', args.evaluate)        
-        validate(val_loader, model)
+        val(val_loader, model)
         return
     logger.print('frame attention network (fan) afew dataset, learning rate: {:}'.format(args.lr))
     
