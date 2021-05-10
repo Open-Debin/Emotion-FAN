8caf946d2cfeed93ece19e5385d37a05a5cc449f
Debin Meng <52323137+Open-Debin@users.noreply.github.com>
2021-05-10 09:15:17 +0800

    Update baseline_afew.py

--- baseline_afew.py
+++ baseline_afew.py
@@ -42,7 +42,7 @@ def main():
     ''' Train & Eval '''
     if args.evaluate == True:
         logger.print('args.evaluate: {:}', args.evaluate)
-        val(val_loader, model)
+        val(val_loader, model, logger)
         return
     logger.print('baseline afew dataset, learning rate: {:}'.format(args.lr))
 
